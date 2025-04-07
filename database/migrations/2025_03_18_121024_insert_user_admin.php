7e4840d798ddfd1df8b9cd6af5e078166df8204a
Vithor Coelho <j.vithor13@gmail.com>
2025-04-07 07:55:17 -0300

    Update 2025_03_18_121024_insert_user_admin.php

--- database/migrations/2025_03_18_121024_insert_user_admin.php
+++ database/migrations/2025_03_18_121024_insert_user_admin.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number', 50);
-            $table->tinyInteger('is_active')->default(1);
+            $table->string('phone_number', 50)->nullable;
+            $table->tinyInteger('is_active')->default(1)->nullable;
         });
         
         $user_admin = User::create([
